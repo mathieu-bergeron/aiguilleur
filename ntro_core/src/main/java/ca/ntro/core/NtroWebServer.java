b319de9bc4d1742da93f5b004d9a839504af3c42
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-06-16 14:19:26 -0400

    On veut un NtroWebServer configurable. L'App Ntro devrait pas avoir à créer son serveur

---
+++ ntro_core/src/main/java/ca/ntro/core/NtroWebServer.java
@@ -0,0 +1,11 @@
+package ca.ntro.core;
+
+public class NtroWebServer {
+
+	public static void launchServer(Class<? extends NtroBackend> backendClass, 
+			                           Class<? extends NtroAppWeb> appClass, 
+			                           String[] args) {
+
+	}
+
+}
