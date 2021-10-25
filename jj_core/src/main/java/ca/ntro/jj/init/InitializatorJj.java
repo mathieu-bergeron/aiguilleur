b57d9b0b88ed34031e81e1bde8351a3f5fc7b5ae
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-10-25 10:20:45 -0400

    en cours) initialisation de T.trace (Jdk vs JSweet)

--- jj_core/src/main/java/ca/ntro/jj/init/InitializatorJj.java
+++ jj_core/src/main/java/ca/ntro/jj/init/InitializatorJj.java
@@ -1,15 +1,46 @@
 package ca.ntro.jj.init;
 
 import ca.jj.core.task_graph.TaskGraph;
+import ca.ntro.jj.core.identifyers.ClassId;
+import ca.ntro.jj.core.identifyers.ObjectId;
+import ca.ntro.jj.core.services.TracerJj;
+import ca.ntro.jj.initialization.DependencyRegistrar;
+import ca.ntro.jj.tasks.base.Task;
 import ca.ntro.jj.wrappers.future.Future;
 
-public class InitializatorJj implements Initializator {
+public abstract class InitializatorJj implements Initializator {
 	
 	private InitializationOptions options = new InitializationOptionsJj();
 
 	private TaskGraph buildGraph() {
+		
+		
+		TracerJj tracer = new TracerJj();
+		
+		/* According to TracerJj dependencies
+		 * add a dependency to the initialization task
+		 * provided by the Jdk or JSweet initializor
+		 */
+		tracer.registerDependencies(new DependencyRegistrar() {
+			
+			@Override
+			public void addDependency(ObjectId<? extends Object> objectId) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void addDependency(ClassId<? extends Object> classId) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		return null;
 	}
+	
+	protected abstract Task provideInitializationTask(ObjectId<? extends Object> objectId);
+	protected abstract Task provideInitializationTask(ClassId<? extends Object> classId);
 
 	@Override
 	public Future<Void> execute() {
