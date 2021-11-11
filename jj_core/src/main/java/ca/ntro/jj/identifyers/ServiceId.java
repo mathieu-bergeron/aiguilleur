13fa9e32b36fc8f5e8461f2e509b0d4f41ea1201
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-11-10 19:45:57 -0500

    mineur

--- jj_core/src/main/java/ca/ntro/jj/identifyers/ServiceId.java
+++ jj_core/src/main/java/ca/ntro/jj/identifyers/ServiceId.java
@@ -11,7 +11,7 @@ public class ServiceId<S extends Service<S>> extends ClassId<S> {
 	public static <S extends Service<S>> ServiceId<S> fromServiceClass(Class<S> _class){
 		ServiceId<S> serviceId = new ServiceId<S>(_class);
 		
-		serviceId.setEntityPath(Path.fromClassname(_class.getSimpleName()));
+		serviceId.setEntityPath(Path.fromSingleName(_class.getSimpleName()));
 
 		return serviceId;
 	}
