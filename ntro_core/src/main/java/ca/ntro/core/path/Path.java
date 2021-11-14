6525b17dbb990c90e426976ba8e418f636847e81
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-11-14 10:14:08 -0500

    en cours) PathGeneric comme un exemple d'interface générique

--- ntro_core/src/main/java/ca/ntro/core/path/Path.java
+++ ntro_core/src/main/java/ca/ntro/core/path/Path.java
@@ -1,5 +1,10 @@
 package ca.ntro.core.path;
 
 public interface Path extends PathGeneric<Path> {
+	
+    public static final String FILENAME_SEPARATOR = "¤";
+    public static final String PATH_SEPARATOR = "/";
+    public static final String HTML_ID_SEPARATOR = "-";
+    public static final String CLASSNAME_SEPARATOR = ".";
 
 }
--- ntro_core/src/main/java/ca/ntro/core/path/PathPatternNtro.java
+++ ntro_core/src/main/java/ca/ntro/core/path/PathPatternNtro.java
@@ -1,59 +1,17 @@
 package ca.ntro.core.path;
 
-import java.util.List;
-
 import ca.ntro.core.util.ArrayUtils;
-import ca.ntro.core.util.ListUtils;
 import ca.ntro.core.validation.Validator;
 
-public class PathPatternNtro extends PathNtro implements PathPattern {
-	
+public class PathPatternNtro extends PathGenericNtro<PathPattern, PathPatternNtro> implements PathPattern {
 
-	public PathPatternNtro() {
-		super();
-	}
-
-	protected PathPatternNtro(List<String> names) {
-		super(names);
-	}
-
-	protected PathPatternNtro(PathPattern otherPattern) {
-		super(otherPattern);
+	@Override
+	protected PathPatternNtro newInstance() {
+		return new PathPatternNtro();
 	}
 
 	@Override
-	protected String[] validNameCharacters() {
-		String[] validNameCharacters = ArrayUtils.addString(Validator.validIdCharacters, NAME_WILDCARD);
-		validNameCharacters = ArrayUtils.addString(validNameCharacters, SUBPATH_WILDCARD);
-
-		return validNameCharacters;
-	}
-
-
-	@Override
-	public PathPattern clone() {
-		return subPath(0, nameCount());
-	}
-
-	@Override
-	public PathPattern subPath(int beginIndex) {
-		return subPath(beginIndex, nameCount());
-	}
-
-	@Override
-	public PathPattern subPath(int beginIndex, int endIndexExclusive) {
-		PathPattern path = null;
-		
-		if(ifValidIndices(beginIndex, endIndexExclusive-1)) {
-
-			path = new PathPatternNtro(ListUtils.subList(getNames(), beginIndex, endIndexExclusive));
-
-		}else {
-
-			path = new PathPatternNtro();
-
-		}
-		
-		return path;
+	protected String[] validCharacters() {
+		return ArrayUtils.addString(Validator.validIdCharacters, PathPattern.NAME_WILDCARD);
 	}
 }
