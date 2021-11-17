3e23c054cf0d66ec2dd80b257af9964ccbfc2392
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-11-17 14:57:30 -0500

    TODO) HierarchicalGraph

--- ntro_core/src/main/java/ca/ntro/core/graphs/writers/HierarchicalDagWriterNtro.java
+++ ntro_core/src/main/java/ca/ntro/core/graphs/writers/HierarchicalDagWriterNtro.java
@@ -4,7 +4,6 @@ import ca.ntro.core.graphs.Edge;
 import ca.ntro.core.graphs.EdgeValue;
 import ca.ntro.core.graphs.NodeValue;
 import ca.ntro.core.graphs.hierarchical_dag.HierarchicalDag;
-import ca.ntro.core.graphs.hierarchical_dag.HierarchicalNode;
 import ca.ntro.core.identifyers.StorageId;
 
 public abstract class HierarchicalDagWriterNtro<NV extends NodeValue, EV extends EdgeValue> extends DagWriterNtro<NV,EV> implements HierarchicalDagWriter<NV,EV> {
