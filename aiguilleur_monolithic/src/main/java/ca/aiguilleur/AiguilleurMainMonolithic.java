fb7960c3c588f11775099c06b4d424e73d83162e
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-11-21 10:28:21 -0500

    ajout) aiguilleur_monolithic (Frontend ET Backend en local)

---
+++ aiguilleur_monolithic/src/main/java/ca/aiguilleur/AiguilleurMainMonolithic.java
@@ -0,0 +1,12 @@
+package ca.aiguilleur;
+
+import ca.ntro.core.static_imports.NtroJdk;
+import ntro_app_fx.NtroAppFx;
+
+public class AiguilleurMainMonolithic extends AiguilleurMainFx implements NtroAppFx {
+	
+	public static void main(String[] args) throws Throwable {
+		NtroJdk.initializer().executeBlocking();
+	}
+
+}
