8495a8872608f3a43021f0d6439d2122ccfeeadc
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-11-22 08:30:10 -0500

    en corus

--- ntro_app_fx/src/main/java/ca/ntro/app/AppFxWrapper.java
+++ ntro_app_fx/src/main/java/ca/ntro/app/AppFxWrapper.java
@@ -3,7 +3,7 @@ package ca.ntro.app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class AppFxWrapper extends Application {
+class AppFxWrapper extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
