a60477eb52fae539a70a803dd8438598d59f626c
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-12-05 18:55:01 -0500

    en cours) Stream pour startNodes, nodes et edges

--- ntro_core/src/main/java/ca/ntro/core/graphs/generic_graph/GenericGraph.java
+++ ntro_core/src/main/java/ca/ntro/core/graphs/generic_graph/GenericGraph.java
@@ -11,6 +11,7 @@ import ca.ntro.core.graphs.NodeReducer;
 import ca.ntro.core.graphs.NodeVisitor;
 import ca.ntro.core.graphs.SearchOptionsBuilder;
 import ca.ntro.core.graphs.writers.GraphWriter;
+import ca.ntro.core.stream.Stream;
 import ca.ntro.core.wrappers.result.Result;
 
 public interface GenericGraph<N extends Node<N,E,SO>, 
@@ -28,12 +29,18 @@ public interface GenericGraph<N extends Node<N,E,SO>,
 	N findNode(NodeId nodeId);
 
 	//E findEdge(NodeId fromId, String edgeName, NodeId toId);
+	
+	Stream<N> startNodes();
 
 	void forEachStartNode(NodeVisitor<N,E,SO> visitor);
 	<R extends Object> Result<R> reduceStartNodes(R initialValue, NodeReducer<N,E,SO,R> reducer);
 
+	Stream<N> nodes();
+
 	void forEachNode(NodeVisitor<N,E,SO> visitor);
 	<R extends Object> Result<R> reduceNodes(R initialValue, NodeReducer<N,E,SO,R> reducer);
+
+	Stream<E> edges();
 
 	void forEachEdge(EdgeVisitor<N,E,SO> visitor);
 	<R extends Object> Result<R> reduceEdges(R initialValue, EdgeReducer<N,E,SO,R> reducer);
