92ce084fe642f801760fc88b0b758027c20f13d2
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-12-07 07:27:52 -0500

    débogage

--- ntro_core_test/src/main/java/ca/ntro/core/graphs/graph/GraphTests.java
+++ ntro_core_test/src/main/java/ca/ntro/core/graphs/graph/GraphTests.java
@@ -32,13 +32,12 @@ public class GraphTests {
 		builder.setGraphName("simpleGraph01");
 		
 		GraphNodeBuilder<MockNode, MockEdge> nodeA = builder.addNode("A");
-		GraphNodeBuilder<MockNode, MockEdge> nodeB = builder.addNode("B");
 
-		//GraphNodeBuilder<MockNode, MockEdge> nodeB = builder.addNode(new MockNode("B"));
+		GraphNodeBuilder<MockNode, MockEdge> nodeB = builder.addNode(new MockNode("B"));
 		
 		//MockNode _nodeA = nodeA.node();
 
-		//MockEdge edgeAA = builder.addEdge(nodeA, "AA", nodeA);
+		MockEdge edgeAA = builder.addEdge(nodeA, "AA", nodeA);
 
 		//nodeA.addEdge(edgeAA);
 
