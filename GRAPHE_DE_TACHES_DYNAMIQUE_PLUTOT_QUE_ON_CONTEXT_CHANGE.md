cc5733a3911e15621af6788996fb19617f8ee737
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-01-12 04:53:55 -0500

    TODO) remplacer onContextChange par un TaskGraph dynamique

---
+++ GRAPHE_DE_TACHES_DYNAMIQUE_PLUTOT_QUE_ON_CONTEXT_CHANGE.md
@@ -0,0 +1,9 @@
+# TaskGraph dynamique plûtot que onContextChange
+
+On peut tout gérer via un graphe de tâches dynamique:
+
+* Changement de locale (langue)
+* Changement de session (sur déconnexion, par exemple)
+* Changement d'usager (c'est un modèle! p.ex. UserBySessionId)
+
+
--- ntro_app_fx/src/main/java/ca/ntro/app/frontend/ViewRegistrarFx.java
+++ ntro_app_fx/src/main/java/ca/ntro/app/frontend/ViewRegistrarFx.java
@@ -14,49 +14,9 @@ import static ca.ntro.app.frontend.tasks.Factory.*;
 
 public class ViewRegistrarFx implements ViewRegistrar<ViewFx>, ViewRegistrarAccessor<ViewFx> {
 
-	private Scene rootScene;
-	
-	private Map<Class<? extends View>, Scene> scenes = new HashMap<>();
+	private Map<Class<? extends View>, String> fxmlFiles = new HashMap<>();
 	private Map<Class<? extends View>, ViewFx> views = new HashMap<>();
 
-	private Map<Class<? extends View>, String> fxmlFiles = new HashMap<>();
-
-	public Scene rootScene() {
-		return rootScene;
-	}
-
-	public Scene scene(Class<? extends View> viewClass) {
-		return scenes.get(viewClass);
-	}
-	
-	/*
-	public void registerRootView(String fxmlPath, 
-			                     int width, 
-			                     int height) {
-
-		URL xmlFile = ViewRegistrarFx.class.getResource(fxmlPath);
-		
-		if(xmlFile == null) {
-			throw new RuntimeException("Not found " + fxmlPath);
-		}
-		
-		FXMLLoader loader = new FXMLLoader(xmlFile);
-
-		rootView = (RootViewFx) loader.getController();
-		
-		Parent parent;
-		try {
-
-			parent = loader.load();
-
-		} catch (IOException e) {
-
-			throw new RuntimeException(e);
-		}
-		
-		rootScene = new Scene(parent, width, height);
-	}
-	*/
 
 	public void registerView(Class<? extends View> viewClass, String fxmlPath) {
 		fxmlFiles.put(viewClass, fxmlPath);
@@ -68,7 +28,7 @@ public class ViewRegistrarFx implements ViewRegistrar<ViewFx>, ViewRegistrarAcce
 	public void registerStylesheet(String cssPath) {
 	}
 
-	public void registerTranslatedStrings(String lang, String propertiesPath) {
+	public void registerTranslatedStrings(Locale locale, String propertiesPath) {
 	}
 
 	@Override
